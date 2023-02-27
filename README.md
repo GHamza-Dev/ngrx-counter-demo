82dc76cf668de59b2d53e85df24a650ee7b25ed4
GHamza-Dev <elgassaihamzam54@gmail.com>
2023-02-27 15:36:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngrx-counter-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-snj7qq)
---
+++ src/app/store/counter.actions.ts
@@ -0,0 +1,5 @@
+import { createAction } from '@ngrx/store';
+
+export const increment = createAction('[COUNTER COMPONENT] Increment');
+export const decrement = createAction('[COUNTER COMPONENT] Decrement');
+export const reset = createAction('[COUNTER COMPONENT] Reset');
